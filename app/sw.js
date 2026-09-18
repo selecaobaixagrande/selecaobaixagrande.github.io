a2221b69a5f514ba068308fe5fdd93b957a51b85
valevini025-dot <valevini025@gmail.com>
2026-09-18 14:08:30 -0300

    Refresh app service worker cache

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE='selecaobg-app-v19';
+const CACHE='selecaobg-app-v20';
 const SHELL=['./','./index.html','./style.css?v=19','./app.js?v=19','./manifest.json','../1000001685.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
