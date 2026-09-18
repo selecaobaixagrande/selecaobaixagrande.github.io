4bead6597ac34b664b06c02bb6046ac2b10fa42d
valevini025-dot <valevini025@gmail.com>
2026-09-18 14:56:52 -0300

    Force refresh fixed call controls cache

--- app/sw.js
+++ app/sw.js
@@ -1,5 +1,5 @@
-const CACHE='selecaobg-app-v23';
-const SHELL=['./','./index.html','./style.css?v=23','./app.js?v=23','./manifest.json','./calls-enhancements.js?v=2','../1000001685.png'];
+const CACHE='selecaobg-app-v24';
+const SHELL=['./','./index.html','./style.css?v=24','./app.js?v=24','./manifest.json','./calls-enhancements.js?v=3','../1000001685.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
