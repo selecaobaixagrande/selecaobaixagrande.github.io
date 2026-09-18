5d64cc102cb1a210594a05b4542d52c837f498f8
valevini025-dot <valevini025@gmail.com>
2026-09-18 14:28:58 -0300

    Atualizar cache do aplicativo para v21

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE='selecaobg-app-v20';
+const CACHE='selecaobg-app-v21';
 const SHELL=['./','./index.html','./style.css?v=20','./app.js?v=20','./manifest.json','../1000001685.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
