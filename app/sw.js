0ea2fc389d8d773d1616352dee8d3afa8f65e956
valevini025-dot <valevini025@gmail.com>
2026-09-18 20:01:07 -0300

    Align service worker app asset with v39

--- app/sw.js
+++ app/sw.js
@@ -1,5 +1,5 @@
 const CACHE='selecaobg-app-v39';
-const SHELL=['./','./index.html','./style.css?v=36','./app.js?v=37','./manifest.json','../1000001685.png'];
+const SHELL=['./','./index.html','./style.css?v=36','./app.js?v=39','./manifest.json','../1000001685.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
