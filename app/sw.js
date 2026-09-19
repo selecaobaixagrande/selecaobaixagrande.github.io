bec4db5e27f1333203c1220cfb2dedc21bfea043
valevini025-dot <valevini025@gmail.com>
2026-09-19 10:16:01 -0300

    Atualizar cache da correção mobile definitiva

--- app/sw.js
+++ app/sw.js
@@ -1,5 +1,5 @@
-const CACHE='selecaobg-app-v43';
-const SHELL=['./','./index.html','./style.css?v=38','./app.js?v=41','./manifest.json','./hero.jpg','../1000001685.png'];
+const CACHE='selecaobg-app-v44';
+const SHELL=['./','./index.html','./style.css?v=39','./app.js?v=41','./manifest.json','./hero.jpg','../1000001685.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
