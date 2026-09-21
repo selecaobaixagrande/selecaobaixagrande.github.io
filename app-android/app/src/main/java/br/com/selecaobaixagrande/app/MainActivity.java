75872cd7b405b47e91e01f52767905a7faf364a9
valevini025-dot <valevini025@gmail.com>
2026-09-20 21:37:27 -0300

    build: atualizar APK com a correção mais recente da presença

--- app-android/app/src/main/java/br/com/selecaobaixagrande/app/MainActivity.java
+++ app-android/app/src/main/java/br/com/selecaobaixagrande/app/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends Activity {
         settings.setDatabaseEnabled(true);
         settings.setAllowFileAccess(true);
         settings.setAllowContentAccess(true);
-        webView.loadUrl("https://selecaobaixagrande.github.io/comissao.html?v=20260921-01");
+        webView.loadUrl("https://selecaobaixagrande.github.io/comissao.html?v=20260921-02");
         setContentView(webView);
     }
 
