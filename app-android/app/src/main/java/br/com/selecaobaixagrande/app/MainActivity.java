fe7660d26c6c604ae9c028c5bdaec484fc480cf4
valevini025-dot <valevini025@gmail.com>
2026-09-20 21:12:35 -0300

    build: atualizar APK para nova versão da comissão

--- app-android/app/src/main/java/br/com/selecaobaixagrande/app/MainActivity.java
+++ app-android/app/src/main/java/br/com/selecaobaixagrande/app/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends Activity {
         settings.setDatabaseEnabled(true);
         settings.setAllowFileAccess(true);
         settings.setAllowContentAccess(true);
-        webView.loadUrl("https://selecaobaixagrande.github.io/comissao.html?v=20260920-9");
+        webView.loadUrl("https://selecaobaixagrande.github.io/comissao.html?v=20260920-10");
         setContentView(webView);
     }
 
