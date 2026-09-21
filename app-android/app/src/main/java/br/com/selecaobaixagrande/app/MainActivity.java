3b665800f07efbac9035ff887de5c4f68edf0a65
valevini025-dot <valevini025@gmail.com>
2026-09-20 21:26:45 -0300

    build: atualizar APK com a correção dos grupos

--- app-android/app/src/main/java/br/com/selecaobaixagrande/app/MainActivity.java
+++ app-android/app/src/main/java/br/com/selecaobaixagrande/app/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends Activity {
         settings.setDatabaseEnabled(true);
         settings.setAllowFileAccess(true);
         settings.setAllowContentAccess(true);
-        webView.loadUrl("https://selecaobaixagrande.github.io/comissao.html?v=20260920-10");
+        webView.loadUrl("https://selecaobaixagrande.github.io/comissao.html?v=20260921-01");
         setContentView(webView);
     }
 
